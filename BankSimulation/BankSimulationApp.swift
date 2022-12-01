abfae9fa089528673d9b6eb7e308c5ecc9514237
Denis Beloshitskiy <beloshitskiy.denis@gmail.com>
2022-12-01 16:36:49 +0300

    Initial Commit

---
+++ BankSimulation/BankSimulationApp.swift
@@ -0,0 +1,17 @@
+//
+//  BankSimulationApp.swift
+//  BankSimulation
+//
+//  Created by Denis Beloshitskiy on 12/1/22.
+//
+
+import SwiftUI
+
+@main
+struct BankSimulationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
